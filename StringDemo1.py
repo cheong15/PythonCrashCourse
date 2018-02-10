8df368fef3dd466d0ff5e61e73f2a2b1fa70faff
cheong15 <cheonglee123@163.com>
2018-02-10 20:11:47 +0800

    string

---
+++ StringDemo1.py
@@ -0,0 +1,17 @@
+# 以首字母大写方式转换
+name = "ada lovelace"
+# print(name.title())
+
+# 换行
+# print('a\nb')
+
+# 删除空白
+# favorite_language='python '
+# print(favorite_language.rstrip())
+
+# 使用str()避免类型错误
+# age=23
+# message = 'happy' + str(age) + 'rd Birthday'
+# print(message)
+
+
